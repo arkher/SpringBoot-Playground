8d678777431ee79e72e13c30db52418829807ee9
Paulo Ricardo <paulo.rgomes22@gmail.com>
2020-11-04 16:42:27 -0300

    feat(Pedido service): Adicionadas funções de verificação.

--- src/main/java/br/com/ithappens/service/PedidoService.java
+++ src/main/java/br/com/ithappens/service/PedidoService.java
@@ -3,6 +3,10 @@ package br.com.ithappens.service;
 import br.com.ithappens.mapper.*;
 import br.com.ithappens.model.*;
 import br.com.ithappens.model.enums.TipoPedido;
+import br.com.ithappens.model.exceptions.FilialNaoEncontradaException;
+import br.com.ithappens.model.exceptions.ProdutoInexistenteException;
+import br.com.ithappens.model.exceptions.QuantidadeInvalidaException;
+import br.com.ithappens.model.exceptions.SemItensPedidoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -66,34 +70,55 @@ public class PedidoService {
             }else{
                 return false;
             }
-
         }
         return true;
     }
 
     public Boolean insert(Pedido pedido){
+        verificarPedido(pedido);
         try{
             Filial filial = filialMapper.findOne(pedido.getFilial().getId());
-            if(filial!=null) {
-                pedidoMapper.insert(pedido);
-                List<ItemPedido> itensPedido = pedido.getItensPedido();
-                itemPedidoMapper.insertItensPedido(pedido.getId(), itensPedido);
+            pedidoMapper.insert(pedido);
 
-                if(pedido.getTipoPedido()==TipoPedido.ENTRADA) {
-                    return insertPedidoEntrada(itensPedido, pedido, filial);
-                }else{
-                    return insertPedidoSaida(itensPedido, pedido);
-                }
+            List<ItemPedido> itensPedido = pedido.getItensPedido();
+
+            itemPedidoMapper.insertItensPedido(pedido.getId(), itensPedido);
+            if(pedido.getTipoPedido()==TipoPedido.ENTRADA) {
+                return insertPedidoEntrada(itensPedido, pedido, filial);
+            }else{
+                return insertPedidoSaida(itensPedido, pedido);
             }
+
         }catch(Exception e){ // depois mudar para exceção personalizada
             e.printStackTrace();
             return false;
         }
-        return false;
     }
 
     public void delete(Long id) {
         pedidoMapper.delete(id);
     }
 
+    public void verificarPedido(Pedido pedido){
+        if(pedido.getFilial()==null) throw new FilialNaoEncontradaException("Filial precisa ser informada.");
+
+        Filial filial = filialMapper.findOne(pedido.getFilial().getId());
+        if(filial==null) throw new FilialNaoEncontradaException("Filial do pedido não encontrada.");
+        if(pedido.getItensPedido().isEmpty() || pedido.getItensPedido()==null)
+            throw new SemItensPedidoException("Lista de itens do pedido não pode ser vazia.");
+        if(!(pedido.getTipoPedido().equals(TipoPedido.ENTRADA) || pedido.getTipoPedido().equals(TipoPedido.SAIDA)))
+            throw new FilialNaoEncontradaException("Tipo de pedido inválido");
+
+        List<ItemPedido> itensPedido = pedido.getItensPedido();
+        verificarItensPedido(itensPedido);
+    }
+
+    public void verificarItensPedido(List<ItemPedido> itensPedido) {
+        for (ItemPedido itemPedido : itensPedido) {
+            if(itemPedido.getIdProduto() == null) throw new ProdutoInexistenteException("O id do produto precisa ser informado");
+            Produto prod = produtoMapper.findOne(itemPedido.getIdProduto());
+            if(prod == null) throw new ProdutoInexistenteException("Há produtos não cadastrados.");
+            if(itemPedido.getQuantidade() == null || itemPedido.getQuantidade()<0) throw new QuantidadeInvalidaException("Há produtos com quantidades inválidas");
+        }
+    }
 }
